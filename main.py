ee0b2acdee902cb8b2035da2fe2e913b746bf704
Ar3455 <72777429+Ar3455@users.noreply.github.com>
2020-12-10 13:11:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 while True:
-    print("Light level:"+input.light_level())
-    if input.light_level():
-        light.set_all(light.rgb(0, 0, 255))
+    print("Light Levels:" + input.light_level())
+    if input.light_level() < 6:
+        light.set_all(light.rgb(255, 0, 255))
+    elif input.light_level() < 13:
+     light.set_all(light.rgb(255, 0, 0))
 else:
-    light.set_all(light.rgb(0, 0, 255 ))
+  light.clear()
